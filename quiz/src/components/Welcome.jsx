54f8102e6750fabf07b6fb0144219734f6e910da
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2022-12-28 21:55:08 +0000

    2 commit

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -1,10 +1,17 @@
-import Quiz from "../img/quiz.svg";
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import Quiz from "../img/quiz.png";
 
 import "./Welcome.css";
 
 const Welcome = () => {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState);
+
   return (
-    <div>
+    <div id="welcome">
         <h2>Seja Bem-Vindo</h2>
         <p>Clique no botao abaixo para comecar:</p>
         <button>Iniciar</button>
