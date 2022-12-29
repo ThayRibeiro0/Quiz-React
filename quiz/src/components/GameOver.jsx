719f313ddd790ce6ed9316bb51130aa60742f1c9
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2022-12-29 04:18:53 +0000

    5 committt

--- quiz/src/components/GameOver.jsx
+++ quiz/src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 import { useContext} from 'react';
 import { QuizContext } from '../context/quiz';
 
-import WellDone from '../img/welldone.svg';
+import WellDone from '../img/gameover.png';
 
 const GameOver = () => {
   const [quizState, dispatch] = useContext(QuizContext);
